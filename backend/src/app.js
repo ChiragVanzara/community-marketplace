cbff8bd4ecb268b6a6ee8e22e727e48fadb5259c
kirtan0101 <kjpatel200022@gmail.com>
2024-12-20 01:21:59 +0530

    backend error resolve

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,27 @@
 import express from "express";
 import cors from "cors";
-import path from "path"
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Derive __dirname equivalent
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(
-    cors({
-      origin: process.env.CORS_ORIGIN,
-      credentials: true,
-    }),
-  );
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
+// Serve static files
 app.use(express.static(path.join(__dirname, "dist")));
 
+// Handle all other routes
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 export { app };
