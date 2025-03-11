6a616022c71cb240ae730efdd37a5bd22b0eb43b
Kirtan Patel <159251485+kirtanpatel01@users.noreply.github.com>
2025-03-11 11:42:25 +0530

    Delete backend directory

--- backend/src/routes/notification.routers.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { verifyJWT } from "../middlewares/auth.middlewares.js";
-import { sendBuyNotification } from "../controllers/notification.controllers.js";
-
-const router = Router();
-
-router.route('/notify-buy/:notificationId').post(verifyJWT, sendBuyNotification);
-
-export default router;
--- backend/src/utils/asyncHandler.js
+++
@@ -1,7 +0,0 @@
-const asyncHandler = (requestHanlder) => {
-    return (req, res, next) => {
-        Promise.resolve(requestHanlder(req, res, next)).catch((error) => next(error))
-    }
-}
-
-export { asyncHandler };
